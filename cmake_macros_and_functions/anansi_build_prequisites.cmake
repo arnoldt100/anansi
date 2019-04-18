c6df6392d301fa377af29e01c11e3700734f1bb9
arnoldt <arnoldt@ornl.gov>
2019-04-17 22:02:49 -0400

    Initial tracking of file "anansi_build_prequisites.cmake".

---
+++ cmake_macros_and_functions/anansi_build_prequisites.cmake
@@ -0,0 +1,9 @@
+#-----------------------------------------------------
+# This function verifies the key environmental       -
+# variables are set.                                 -
+#                                                    -
+#                                                    -
+#-----------------------------------------------------
+function(verify_key_environmental_are_set)
+    message("Stud for checking key environmental are set.")
+endfunction()
