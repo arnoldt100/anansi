5b7c593df5acc70f28aca0977c74b586678af7b8
arnoldt <arnoldt@ornl.gov>
2019-04-17 22:26:21 -0400

    More modifications to get the basic CMmake configurations to work.

--- configurations/Catalan.sh
+++ configurations/Catalan.sh
@@ -1,2 +1,16 @@
 #! /usr/bin/env bash
+
+#-----------------------------------------------------
+# The minimim cmake version required to build        -
+# this project.                                      -
+#                                                    -
+# This variable is mandatory.                        -
+#-----------------------------------------------------
 export ANANSI_MINIMUM_CMAKE_VERSION="3.10"
+
+#-----------------------------------------------------
+# Define the path to Anansi cmake modules.           -
+#                                                    -
+# This variable is mandatory.                        -
+#-----------------------------------------------------
+export ANANSI_CMAKE_MODULE_PATH=cmake_macros_and_functions
