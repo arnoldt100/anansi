c64725f14719f32697ed547eb5a795fc1de20825
arnoldt <arnoldt@ornl.gov>
2019-05-11 15:39:10 -0400

    The anansi_main_md program now instantiates a molecular dynamics class.

--- sources/molecular_dynamics/header_files/anansi_main_md.h
+++ sources/molecular_dynamics/header_files/anansi_main_md.h
@@ -1,1 +1,6 @@
+// System includes
+
+// Local includes
+#include "anansi_molecular_dynamics.h"
+
 int main(int argc, char** argv);
--- sources/molecular_dynamics/implementation_files/anansi_main_md.cpp
+++ sources/molecular_dynamics/implementation_files/anansi_main_md.cpp
@@ -2,5 +2,13 @@
 
 int main( int argc, char** argv )
 {
+    ANANSI::MolecularDynamics * md_ptr = new ANANSI::AnansiMolecularDynamics;
+
+    if (md_ptr != nullptr)
+    {
+        delete md_ptr;
+        md_ptr=nullptr;
+    }
+
     return 0;
 }
