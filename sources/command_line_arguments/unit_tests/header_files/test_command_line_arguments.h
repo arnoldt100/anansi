050c8d5039ea771ddaa41f7fbc625e676d8e4395
Arnold <arnoldt@ornl.gov>
2020-04-15 17:17:20 -0400

    Step 1 in adding skeletal tree of CommandLineArguments testing tree.
    Addded a stud test for checking the number of command line arguments.
    Addded a stud test for checking the veracity of the argument string.

--- sources/command_line_arguments/unit_tests/header_files/test_command_line_arguments.h
+++ sources/command_line_arguments/unit_tests/header_files/test_command_line_arguments.h
@@ -3,13 +3,33 @@
 
 
 #define BOOST_TEST_MODULE Command Line Arguments
-#define BOOST_TEST_NO_MAIN
-#define BOOST_TEST_ALTERNATIVE_INIT_API
 
 #include <boost/test/included/unit_test.hpp>
 #include <iostream>
-
 namespace utf = boost::unit_test;
 
+class CLAFixture 
+{
+    public:
+        CLAFixture()
+        {
+           return; 
+        }
+
+        ~CLAFixture()
+        {
+           return; 
+        }
+
+        void setup()
+        {
+            std::cout << "setup of CLAFixture." << std::endl;
+        }
+
+        void teardown()
+        {
+            std::cout << "teardown of CLAFixture." << std::endl;
+        }
+};
 
 #endif   // ----- #ifndef test_command_line_arguments_INC  ----- 
--- sources/command_line_arguments/unit_tests/implementation_files/test_command_line_arguments.cpp
+++ sources/command_line_arguments/unit_tests/implementation_files/test_command_line_arguments.cpp
@@ -1,11 +1,20 @@
 
 #include "test_command_line_arguments.h"
 
+// Create a fixture for initializing a
+// CommandLineArguments class
+
 BOOST_AUTO_TEST_SUITE( Test_Suite_CommandLineArguments )
+
+BOOST_FIXTURE_TEST_CASE( test_number_of_arguments,CLAFixture )
+{
+  BOOST_TEST( 2 < 1, "The number of test arguments is incorrect" );
+}
+
+BOOST_FIXTURE_TEST_CASE( test_arguments,CLAFixture )
+{
+  BOOST_TEST(2 < 1, "The arguments are incorrect.");
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
-int main (int argc, char* argv[], char* envp[])
-{
-  return utf::unit_test_main(init_unit_test, argc, argv);
-}
