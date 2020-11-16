f779351f9298b1e3d3f5bcc08046d85602f04a4f
Arnold Tharringtom <arnoldt@ornl.gov>
2020-11-16 00:11:26 -0500

    Tracking file string_utilities_build_prerequisites.cmake.

--- sources/string_utilities/cmake_macros_and_functions/string_utilities_build_prerequisites.cmake
+++ sources/string_utilities/cmake_macros_and_functions/string_utilities_build_prerequisites.cmake
@@ -8,41 +8,41 @@ function(verify_key_environmental_are_set)
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # mpi_communicator_install_lib_directory             -
+    # string_utilities_install_lib_directory             -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED mpi_communicator_install_lib_directory)
-        set(log_message "mpi_communicator_install_lib_directory=${mpi_communicator_install_lib_directory}\n")
+    if(DEFINED string_utilities_install_lib_directory)
+        set(log_message "string_utilities_install_lib_directory=${string_utilities_install_lib_directory}\n")
         file(APPEND ${anansi_log_file} "${log_message}")
     else()
-        message( FATAL_ERROR "The variable mpi_communicator_install_lib_directory is not defined. This \
-        variable defines the directory to install the library mpi_communicator." )
+        message( FATAL_ERROR "The variable string_utilities_install_lib_directory is not defined. This \
+        variable defines the directory to install the library string_utilities." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # mpi_communicator_install_include_directory         -
+    # string_utilities_install_include_directory         -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED mpi_communicator_install_include_directory)
-        set(log_message "mpi_communicator_install_include_directory=${mpi_communicator_install_include_directory}\n")
+    if(DEFINED string_utilities_install_include_directory)
+        set(log_message "string_utilities_install_include_directory=${string_utilities_install_include_directory}\n")
         file(APPEND ${anansi_log_file} "${log_message}")
     else()
-        message( FATAL_ERROR "The variable mpi_communicator_install_include_directory is not defined. This \
-        variable defines the directory to install the header files for mpi_communicator." )
+        message( FATAL_ERROR "The variable string_utilities_install_include_directory is not defined. This \
+        variable defines the directory to install the header files for string_utilities." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # mpi_communicator_install_bin_directory             -
+    # string_utilities_install_bin_directory             -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED mpi_communicator_install_bin_directory)
-        set(log_message "mpi_communicator_install_bin_directory=${mpi_communicator_install_bin_directory}\n")
+    if(DEFINED string_utilities_install_bin_directory)
+        set(log_message "string_utilities_install_bin_directory=${string_utilities_install_bin_directory}\n")
         file(APPEND ${anansi_log_file} "${log_message}")
     else()
-        message( FATAL_ERROR "The variable mpi_communicator_install_bin_directory is not defined. This \
-        variable defines the directory to install the header files for mpi_communicator." )
+        message( FATAL_ERROR "The variable string_utilities_install_bin_directory is not defined. This \
+        variable defines the directory to install the header files for string_utilities." )
     endif()
 
 
