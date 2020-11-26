1befb33d404fc35c2faff07d0397ed52acb341d3
Arnold Tharringtom <arnoldt@ornl.gov>
2020-11-26 14:24:43 -0500

    Implemented the skeletal framework for simulation parameters.

--- sources/simulation_parameters/unit_tests/header_files/test_simulationparameters.h
+++ sources/simulation_parameters/unit_tests/header_files/test_simulationparameters.h
@@ -2,14 +2,35 @@
 #define  test_simulationparameters_INC
 
 
-#define BOOST_TEST_MODULE Command Line Arguments
-#define BOOST_TEST_NO_MAIN
-#define BOOST_TEST_ALTERNATIVE_INIT_API
+#define BOOST_TEST_MODULE Simulation Parameters
 
 #include <boost/test/included/unit_test.hpp>
 #include <iostream>
 
 namespace utf = boost::unit_test;
 
+class SimulationParametersFixture 
+{
+    public:
+        SimulationParametersFixture()
+        {
+           return; 
+        }
+
+        ~SimulationParametersFixture()
+        {
+           return; 
+        }
+
+        void setup()
+        {
+            std::cout << "setup of SimulationParametersFixture." << std::endl;
+        }
+
+        void teardown()
+        {
+            std::cout << "teardown of SimulationParametersFixture." << std::endl;
+        }
+};
 
 #endif   // ----- #ifndef test_simulationparameters_INC  ----- 
--- sources/simulation_parameters/unit_tests/implementation_files/test_simulationparameters.cpp
+++ sources/simulation_parameters/unit_tests/implementation_files/test_simulationparameters.cpp
@@ -1,11 +1,16 @@
-
 #include "test_simulationparameters.h"
 
-BOOST_AUTO_TEST_SUITE( Test_Suite_CommandLineArguments )
+BOOST_AUTO_TEST_SUITE( Test_Suite_Simulation_Parameters )
+
+BOOST_FIXTURE_TEST_CASE( test_stud_1, SimulationParametersFixture )
+{
+  BOOST_TEST( 2 < 1, "The test test_stud_1 is incorrect" );
+}
+
+BOOST_FIXTURE_TEST_CASE( test_stud_2, SimulationParametersFixture )
+{
+  BOOST_TEST(2 < 1, "The test test_stud_2 is incorrect.");
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
-
-int main (int argc, char* argv[], char* envp[])
-{
-  return utf::unit_test_main(init_unit_test, argc, argv);
-}
