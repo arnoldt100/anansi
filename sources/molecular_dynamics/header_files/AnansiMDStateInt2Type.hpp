02dc3427ba3eb0b5ddbcf0b0ef1c7ec8e30b4489
Arnold Tharrington <arnoldt100@gmail.com>
2021-12-20 16:09:57 -0500

    Initial tracking of new files and perparing to create first object factory.

---
+++ sources/molecular_dynamics/header_files/AnansiMDStateInt2Type.hpp
@@ -0,0 +1,101 @@
+#ifndef  AnansiMDStateInt2Type_INC
+#define  AnansiMDStateInt2Type_INC
+
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI
+{
+
+// =====================================================================================
+//        Class:  AnansiMDStateInt2Type
+//  Description:  
+//  =====================================================================================
+template <int T>
+class AnansiMDStateInt2Type
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        //--------------------------------------------------------------------------------------
+        //       Class:  AnansiMDStateInt2Type
+        //      Method:  AnansiMDStateInt2Type :: AnansiMDStateInt2Type
+        // Description:  
+        // 
+        //  Parameters: 
+        //
+        //      Return:
+        //--------------------------------------------------------------------------------------
+        AnansiMDStateInt2Type ()
+        {
+            return;
+        };   // constructor
+
+        AnansiMDStateInt2Type (const AnansiMDStateInt2Type & other)
+        {
+            return;
+        }   // copy constructor
+
+        AnansiMDStateInt2Type (AnansiMDStateInt2Type && other)
+        {
+            return;
+        }   // copy-move constructor
+
+        ~AnansiMDStateInt2Type ()
+        {
+            return;
+        }  // destructor
+
+        // ====================  ACCESSORS     =======================================
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        AnansiMDStateInt2Type& operator= ( const AnansiMDStateInt2Type &other )
+        {
+            if (this != &other)
+            {
+
+            }
+            return *this;
+        } // assignment operator
+
+        AnansiMDStateInt2Type& operator= ( AnansiMDStateInt2Type && other )
+        {
+            if (this != &other)
+            {
+
+            }
+            return *this;
+        } // assignment-move operator
+
+        // ====================  DATA MEMBERS  =======================================
+        enum { value = T };
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class AnansiMDStateInt2Type  -----
+
+
+}; // namespace ANANSI
+
+#endif   // ----- #ifndef AnansiMDStateInt2Type_INC  ----- 
