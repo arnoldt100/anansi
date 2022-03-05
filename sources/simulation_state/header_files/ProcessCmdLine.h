37408bbef2e893fffefb070673ce4fc7d2936563
Arnold Tharrington <arnoldt100@gmail.com>
2022-03-05 16:59:16 -0500

    Added class ProcessCmdLine

---
+++ sources/simulation_state/header_files/ProcessCmdLine.h
@@ -0,0 +1,71 @@
+#ifndef ANANSI_ProcessCmdLine_INC
+#define ANANSI_ProcessCmdLine_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "SimulationState.h"
+
+namespace ANANSI
+{
+
+     // =====================================================================================
+     //        Class:  ProcessCmdLine
+     //  Description:  
+     //  =====================================================================================
+    class ProcessCmdLine : public SimulationState
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  ProcessCmdLine
+            //      Method:  ProcessCmdLine :: ProcessCmdLine
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            ProcessCmdLine ();   // constructor
+
+            ProcessCmdLine (ProcessCmdLine const & other);   // copy constructor
+
+            ProcessCmdLine (ProcessCmdLine && other);   // copy-move constructor
+
+            virtual ~ProcessCmdLine ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            ProcessCmdLine& operator= ( ProcessCmdLine const & other ); // assignment operator
+
+            ProcessCmdLine& operator= ( ProcessCmdLine && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class ProcessCmdLine  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_ProcessCmdLine_INC
