e52f74dc8d3c703d18ccf7a6b1555bceb0a5e632
Arnold Tharrington <arnoldt100@gmail.com>
2022-03-05 16:21:56 -0500

    Added class InitSimEnv.

---
+++ sources/simulation_state/header_files/InitSimEnv.h
@@ -0,0 +1,71 @@
+#ifndef ANANSI_InitSimEnv_INC
+#define ANANSI_InitSimEnv_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "SimulationState.h"
+
+namespace ANANSI
+{
+
+     // =====================================================================================
+     //        Class:  InitSimEnv
+     //  Description:  
+     //  =====================================================================================
+    class InitSimEnv : public SimulationState
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  InitSimEnv
+            //      Method:  InitSimEnv :: InitSimEnv
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            InitSimEnv ();   // constructor
+
+            InitSimEnv (InitSimEnv const & other);   // copy constructor
+
+            InitSimEnv (InitSimEnv && other);   // copy-move constructor
+
+            virtual ~InitSimEnv ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            InitSimEnv& operator= ( InitSimEnv const & other ); // assignment operator
+
+            InitSimEnv& operator= ( InitSimEnv && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class InitSimEnv  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_InitSimEnv_INC
