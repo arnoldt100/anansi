836de066b3a369e7b4d99a477d82d8477bcb360c
Arnold Tharrington <arnoldt100@gmail.com>
2022-03-05 17:07:10 -0500

    Added class InitInitialConditions

---
+++ sources/simulation_state/header_files/InitInitialConditions.h
@@ -0,0 +1,71 @@
+#ifndef ANANSI_InitInitialConditions_INC
+#define ANANSI_InitInitialConditions_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "SimulationState.h"
+
+namespace ANANSI
+{
+
+     // =====================================================================================
+     //        Class:  InitInitialConditions
+     //  Description:  
+     //  =====================================================================================
+    class InitInitialConditions : public SimulationState
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  InitInitialConditions
+            //      Method:  InitInitialConditions :: InitInitialConditions
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            InitInitialConditions ();   // constructor
+
+            InitInitialConditions (InitInitialConditions const & other);   // copy constructor
+
+            InitInitialConditions (InitInitialConditions && other);   // copy-move constructor
+
+            virtual ~InitInitialConditions ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            InitInitialConditions& operator= ( InitInitialConditions const & other ); // assignment operator
+
+            InitInitialConditions& operator= ( InitInitialConditions && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class InitInitialConditions  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_InitInitialConditions_INC
