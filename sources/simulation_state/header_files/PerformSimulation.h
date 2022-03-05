11f68636e6009afc1df2d3d2a23a31bd53585bb7
Arnold Tharrington <arnoldt100@gmail.com>
2022-03-05 17:17:10 -0500

    Added class PerformSimulation.

---
+++ sources/simulation_state/header_files/PerformSimulation.h
@@ -0,0 +1,71 @@
+#ifndef ANANSI_PerformSimulation_INC
+#define ANANSI_PerformSimulation_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "SimulationState.h"
+
+namespace ANANSI
+{
+
+     // =====================================================================================
+     //        Class:  PerformSimulation
+     //  Description:  
+     //  =====================================================================================
+    class PerformSimulation : public SimulationState
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  PerformSimulation
+            //      Method:  PerformSimulation :: PerformSimulation
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            PerformSimulation ();   // constructor
+
+            PerformSimulation (PerformSimulation const & other);   // copy constructor
+
+            PerformSimulation (PerformSimulation && other);   // copy-move constructor
+
+            virtual ~PerformSimulation ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            PerformSimulation& operator= ( PerformSimulation const & other ); // assignment operator
+
+            PerformSimulation& operator= ( PerformSimulation && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class PerformSimulation  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_PerformSimulation_INC
