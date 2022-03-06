6e63a80c48bd3cd908137bba01b3d38d27b11bc4
Arnold Tharrington <arnoldt100@gmail.com>
2022-03-06 11:40:29 -0500

    Added class MDProcessCmdLine.

---
+++ sources/molecular_dynamics/header_files/MDProcessCmdLine.h
@@ -0,0 +1,71 @@
+#ifndef ANANSI_MDProcessCmdLine_INC
+#define ANANSI_MDProcessCmdLine_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "ProcessCmdLine.h"
+
+namespace ANANSI
+{
+
+     // =====================================================================================
+     //        Class:  MDProcessCmdLine
+     //  Description:  
+     //  =====================================================================================
+    class MDProcessCmdLine : public ProcessCmdLine
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  MDProcessCmdLine
+            //      Method:  MDProcessCmdLine :: MDProcessCmdLine
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            MDProcessCmdLine ();   // constructor
+
+            MDProcessCmdLine (const MDProcessCmdLine & other);   // copy constructor
+
+            MDProcessCmdLine (MDProcessCmdLine && other);   // copy-move constructor
+
+            ~MDProcessCmdLine ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            MDProcessCmdLine& operator= ( const MDProcessCmdLine &other ); // assignment operator
+
+            MDProcessCmdLine& operator= ( MDProcessCmdLine && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class MDProcessCmdLine  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_MDProcessCmdLine_INC
