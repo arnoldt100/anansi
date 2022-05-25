1de170e2255cc9850bb065dae63c0b38110fb1b7
Arnold Tharrington <arnoldt100@gmail.com>
2022-05-25 16:43:48 -0400

    Changed COMMUNICATOR to ANANSI.

--- sources/mpi_communicator/header_files/MPIFinalizedException.h
+++ sources/mpi_communicator/header_files/MPIFinalizedException.h
@@ -5,8 +5,8 @@
  *      Authors: Arnold Tharrington
  */
 
-#ifndef COMMUNICATOR_SOURCE_IMPLEMENTATION_FILES_MPIFINALIZEDEXCEPTION_H_
-#define COMMUNICATOR_SOURCE_IMPLEMENTATION_FILES_MPIFINALIZEDEXCEPTION_H_
+#ifndef ANANSI_MPIFINALIZEDEXCEPTION_H_
+#define ANANSI_MPIFINALIZEDEXCEPTION_H_
 
 //--------------------------------------------------------//
 //-------------------- System includes -------------------//
@@ -17,7 +17,7 @@
 //--------------------- Package includes -----------------//
 //--------------------------------------------------------//
 
-namespace COMMUNICATOR {
+namespace ANANSI {
 
 class MPIFinalizedException : public std::exception {
 
@@ -43,6 +43,6 @@ private:
 
 };
 
-} /* namespace COMMUNICATOR */
+} /* namespace ANANSI */
 
-#endif /* COMMUNICATOR_SOURCE_IMPLEMENTATION_FILES_MPIFINALIZEDEXCEPTION_H_ */
+#endif /* ANANSI_MPIFINALIZEDEXCEPTION_H_ */
