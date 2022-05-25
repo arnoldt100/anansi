f104b65b6feaeb4ab59c1cee0dc4303728533645
Arnold Tharrington <arnoldt100@gmail.com>
2022-05-25 17:46:48 -0400

    Changed COMMUNICATOR to ANANSI.

---
+++ sources/mpi_communicator/header_files/MPIDataType.h
@@ -0,0 +1,61 @@
+/*
+ * MPIDataType.h
+ *
+ *  Created on: 12/15/18
+ *      Authors: Arnold Tharrington
+ */
+
+#ifndef ANANSI_MPIDATATYPE_
+#define ANANSI_MPIDATATYPE_
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+#include "mpi.h"
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI {
+
+template< typename T>
+class MPI_DATA_TYPE;
+
+template<>
+class MPI_DATA_TYPE<int>
+{
+    public:
+        MPI_DATA_TYPE();
+
+        ~MPI_DATA_TYPE();
+
+        static MPI_Datatype value();
+};
+
+template<>
+class MPI_DATA_TYPE<char>
+{
+
+    public:
+        MPI_DATA_TYPE();
+
+        ~MPI_DATA_TYPE();
+
+        static MPI_Datatype value();
+};
+
+template<>
+class MPI_DATA_TYPE<bool>
+{
+
+    public:
+        MPI_DATA_TYPE();
+
+        ~MPI_DATA_TYPE();
+
+        static MPI_Datatype value();
+};
+} /* namespace ANANSI */
+
+#endif /* ANANSI_MPIDATATYPE_ */
