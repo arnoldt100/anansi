04fa794fad7565493124947aa44cf0354840e160
Arnold Tharrington <arnoldt100@gmail.com>
2022-05-26 15:49:47 -0400

    More of the same.

---
+++ sources/mpi_communicator/header_files/MPIBroadcast.h
@@ -0,0 +1,48 @@
+#ifndef  ANANSI_MPIBroadcast_INC
+#define  ANANSI_MPIBroadcast_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+#include <string>
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+#include "mpi.h"
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI
+{
+
+template<typename T>
+class MPI_Broadcast;
+
+template<>
+class MPI_Broadcast<int>
+{
+    public:
+
+    static int Broadcast(const int data_to_broadcast,
+                         const MPI_Comm mpi_comm, const std::size_t bcast_rank);
+
+
+}; // -----  end of class MPI_BROADCAST  -----
+
+
+template<>
+class MPI_Broadcast<std::string>
+{
+    public:
+    static std::string Broadcast(const std::string data_to_broadcast,
+                                 const std::size_t bcast_str_len,
+                                 const MPI_Comm mpi_comm,
+                                 const std::size_t bcast_rank);
+};
+
+}; // namespace ANANSI
+
+#endif   // ----- #ifndef ANANSI_MPIBroadcast_INC  -----
