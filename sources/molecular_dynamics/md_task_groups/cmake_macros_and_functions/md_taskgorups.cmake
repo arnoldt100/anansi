3ba8c4bbbc47676343e6c4c16d341f725440fbd4
Arnold Tharrington <arnoldt100@gmail.com>
2022-06-21 11:42:55 -0400

    Initial commit of md_taskgorups.cmake

---
+++ sources/molecular_dynamics/md_task_groups/cmake_macros_and_functions/md_taskgorups.cmake
@@ -0,0 +1,32 @@
+## =====================================================================================
+##
+##       Filename:  md_taskgroups.cmake
+##
+##    Description:  
+##
+##         Author:  Arnold N. Tharrington (), arnoldt@ornl.gov
+##   Organization:  ORNL-National Center of Computational Sciences
+##
+## =====================================================================================
+
+# ===  MACRO     ======================================================================
+#         Name:  enable_md_task_groups_package
+#  Description:  Defines key environmental variables enabling the
+#                building of the md_tasks_groups packages.
+# 
+#  Arguments: 
+#             install_lib_directory - The location to install the various sub-packages libraries
+#             install_include_directory - The location to install various sub-package header files
+#             install_bin_directory - The location to install sub-package binaraies
+#             cxx_standard - The C++ standard to build the sub-package library and binaries
+# =====================================================================================
+macro ( enable_building_md_task_groups_library install_lib_directory
+        install_include_directory
+        install_bin_directory 
+        cxx_standard)
+
+    set(md_task_groups_install_lib_directory ${install_lib_directory})
+    set(md_task_groups_install_include_directory ${install_include_directory})
+    set(md_task_groups_install_bin_directory ${install_bin_directory} )
+    set(md_task_groups_cxx_standard ${cxx_standard} )
+endmacro()
