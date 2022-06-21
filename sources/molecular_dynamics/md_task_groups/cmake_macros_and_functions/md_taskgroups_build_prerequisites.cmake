b472321d19b47dc69384acaa244ad9ab207fa21d
Arnold Tharrington <arnoldt100@gmail.com>
2022-06-21 11:51:54 -0400

    Initial tracking of md_taskgroups_build_prerequisites.cmake.

---
+++ sources/molecular_dynamics/md_task_groups/cmake_macros_and_functions/md_taskgroups_build_prerequisites.cmake
@@ -0,0 +1,12 @@
+# =====================================================================================
+#
+#       Filename:  md_taskgroups_build_prerequisites.cmake
+#
+#    Description: Contains convenience functions that help verify the
+#                 prerequisites for building subpackages of md_taskgroups
+#                 are satisfied.
+#
+#
+# =====================================================================================
+
+function(verify_md_taskgroups_build_prerequisites_are_set)
