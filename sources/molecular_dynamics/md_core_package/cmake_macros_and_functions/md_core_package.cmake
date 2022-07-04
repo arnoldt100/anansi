5e0848fe7c4b9b01396bd705068086456f4691b4
Arnold Tharrington <arnoldt100@gmail.com>
2022-07-04 10:49:54 -0400

    Modified files to use "md_core" instead of "md_core_package"

---
+++ sources/molecular_dynamics/md_core_package/cmake_macros_and_functions/md_core_package.cmake
@@ -0,0 +1,34 @@
+## =====================================================================================
+##
+##       Filename:  md_core.cmake
+##
+##    Description:  
+##
+##         Author:  Arnold N. Tharrington (), arnoldt@ornl.gov
+##   Organization:  ORNL-National Center of Computational Sciences
+##
+## =====================================================================================
+
+# ===  MACRO     ======================================================================
+#         Name:  enable_building_md_core_package
+#  Description:  Defines key environmental variables enabling the
+#                building of the md_tasks_groups packages.
+# 
+#  Arguments: 
+#             install_lib_directory - The location to install the various libraries
+#             install_include_directory - The location to install various header files
+#             install_bin_directory - The location to install binaraies
+#             cxx_standard - The C++ standard to build the library and binaries
+# =====================================================================================
+macro ( enable_building_md_core_package install_lib_directory
+        install_include_directory
+        install_bin_directory 
+        cxx_standard
+        logfilepath)
+
+    set(md_core_install_lib_directory ${install_lib_directory})
+    set(md_core_install_include_directory ${install_include_directory})
+    set(md_core_install_bin_directory ${install_bin_directory} )
+    set(md_core_cxx_standard ${cxx_standard} )
+    set(md_core_logfilepath ${logfilepath} )
+endmacro()
---
+++ sources/molecular_dynamics/md_core_package/cmake_macros_and_functions/md_core_package_build_prerequisites.cmake
@@ -0,0 +1,52 @@
+# =====================================================================================
+#
+#       Filename:  md_core_build_prerequisites.cmake
+#
+#    Description: Contains convenience functions that help verify the
+#                 prerequisites for building subpackages of md_core
+#                 are satisfied.
+#
+#
+# =====================================================================================
+
+
+# ===  FUNCTION  ======================================================================
+#         Name:  verify_md_core_build_prerequisites_are_met
+#  Description:  Verifies that key variables that control the building and installation
+#                of the package md_core.
+# 
+#   Parameters: 
+#
+#        Return:
+# =====================================================================================
+function(verify_md_core_build_prerequisites_are_met)
+
+    # Ensure that variable "md_core_logfilepath" is defined.
+    if (NOT DEFINED md_core_logfilepath) 
+        message (FATAL_ERROR "md_core_logfilepath is not defined.")
+    endif()
+
+    set(my_message "Verifying \"md_core' build prerequisities\" are satisfied.")
+    log_message_to_file( ${md_core_logfilepath} "---")
+    log_message_to_file( ${md_core_logfilepath} ${my_message})
+
+    # Again verfying that variable 'md_core_logfilepath' is defined ... repeated
+    # for consistency.
+    anansi_test_variable_is_defined("md_core_logfilepath" "${md_core_logfilepath}")
+
+    # Verfying that variable 'md_core_cxx_standard' is defined.
+    anansi_test_variable_is_defined("md_core_cxx_standard"  "${md_core_logfilepath}")
+
+    # Verfying that variable 'md_core_install_bin_directory' is defined.
+    anansi_test_variable_is_defined("md_core_install_bin_directory"  "${md_core_logfilepath}")
+
+    # Verfying that variable 'md_core_install_include_directory' is defined.
+    anansi_test_variable_is_defined("md_core_install_include_directory"  "${md_core_logfilepath}")
+
+    # Verfying that variable 'md_core_install_lib_directory' is defined.
+    anansi_test_variable_is_defined("md_core_install_lib_directory"  "${md_core_logfilepath}")
+
+    set(my_message "Package \"md_core' build prerequisities\" are satisfied.")
+    log_message_to_file( ${md_core_logfilepath} ${my_message})
+    log_message_to_file( ${md_core_logfilepath} "---\n")
+endfunction(verify_md_core_build_prerequisites_are_met)
