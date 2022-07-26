754eeaf25cb877d2a9eb1b355fe350c605027d78
Arnold Tharrington <arnoldt100@gmail.com>
2022-07-26 14:24:17 -0400

    Initial adding of tree_diagream scripts.

---
+++ bin/build_tree_diagram.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+from lib_python import unix_tree_command
+
+def main () :
+    my_tree = unix_tree_command.UnixTreeCommand()
+
+if __name__ == "__main__" :
+
+    main()
+    
---
+++ bin/lib_python/unix_tree_command.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+class UnixTreeCommand:
+
+    def __init__(self) :
+        print("Generating tree diagram.")
+        return
