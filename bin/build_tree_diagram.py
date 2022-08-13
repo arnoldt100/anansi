9820923da4607d44b549f31399f4a07535a46162
Arnold Tharrington <arnoldt100@gmail.com>
2022-08-13 14:03:24 -0400

    Initial tracking of file.

--- bin/build_tree_diagram.py
+++ bin/build_tree_diagram.py
@@ -7,6 +7,7 @@ An image file will be written.
 
 """ 
 
+import argparse
 from lib_python import unix_tree_command
 
 def _exclude_option():
@@ -19,22 +20,53 @@ def _exclude_option():
     exclude_patterns.append('__pycache__')
     exclude_patterns.append('stderr*')
     exclude_patterns.append('stdout*')
+    exclude_patterns.append('anansi_log.txt')
+    exclude_patterns.append('build')
     my_exclude_option = unix_tree_command.CreateExcludeOption(exclude_patterns)
     return my_exclude_option() 
 
+def _get_args():
+    """Returns the program arguments"""
+    parser = argparse.ArgumentParser(description='Process the top-level path to tree diagram')
+
+    required = parser.add_argument_group('required named arguments')
+    required.add_argument('--top-level-dir', 
+                        type=str, 
+                        metavar='DIRPATH',
+                        required=True,
+                        nargs=1,
+                        help='The top-level path of the directory to create a diagram of the list of all files')
+
+
+    args = parser.parse_args()
+    return args
+
 def main () :
-    tree_diagram_options = []
+
+    # Process the command line arguments and get the top-level directory
+    # to generate the list files diagram.
+    args = _get_args()
+    topleveldir=args.top_level_dir[0]
+
+    # We initialize the a list of tree command options. 
+    tree_command_options = []
+
+    # This is the directory 
+    tree_command_options.append(topleveldir)
 
     # This option prunes empty directories
-    tree_diagram_options.append("--prune")
+    tree_command_options.append("--prune")
+
+    # This option outputs XML format
+    tree_command_options.append("-H ./html_files")
 
     # This sections define the files an directories to exclude which
     # we use to create the exclude option.
     my_exclude_option = _exclude_option()
-    tree_diagram_options.append(my_exclude_option)
+    tree_command_options.append(my_exclude_option)
 
-    # Instantaie the tree diagram generator.
-    my_tree = unix_tree_command.UnixTreeCommand(tree_diagram_options)
+    # Instantiate the tree diagram generator and create the diagram
+    my_tree = unix_tree_command.UnixTreeCommand(tree_command_options)
     print(my_tree.tree_command())
     my_tree.create_diagram()
 
