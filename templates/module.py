9e182e1a32a617b7f5c237cec3d7ba5805e211c8
Arnold Tharrington <arnoldt100@gmail.com>
2022-09-08 16:37:01 -0400

    Initial tracking of file "module.py."

---
+++ templates/module.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+## @package __packagename__
+#  __BriefDoxygenDescription__
+#
+# __DetailedDoxygenDescription__
+
+# System imports
+import string
+import argparse
+
+# Local imports
+from loggerutils.logger import create_logger_description
+from loggerutils.logger import create_logger
+
+## @fn main ()
+## @brief The main function.
+def main():
+    args = _parse_arguments()
+
+    logger = _create_logger(log_id='__LoggerID__',
+                           log_level=args.log_level)
+
+    logger.info("Start of main program")
+
+    logger.info("End of main program")
+
+if __name__ == "__main__":
+    main()
+
+## @fn _parse_arguments( )
+## @brief Parses the command line arguments.
+##
+## @details Parses the command line arguments and
+## returns A namespace.
+##
+## @return A namespace. The namespace contains attributes
+##         that are the command line arguments.
+def _parse_arguments():
+
+    # Create a string of the description of the 
+    # program
+    program_description = "Your program description" 
+
+    # Create an argument parser.
+    my_parser = argparse.ArgumentParser(
+            description=program_description,
+            formatter_class=argparse.RawTextHelpFormatter,
+            add_help=True)
+
+    # Add an optional argument for the logging level.
+    my_parser.add_argument("--log-level",
+                           type=int,
+                           default=logging.WARNING,
+                           help=create_logger_description() )
+
+    my_args = my_parser.parse_args()
+
+    return my_args 
+
