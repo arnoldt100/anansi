dc79be06f5ad670fd0cbf06bc0b5f501d6388311
Arnold Tharrington <arnoldt100@gmail.com>
2022-09-19 16:06:42 -0400

    Removing files in docs.

--- docs/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['build_5fdoxygen_5fdocumentation_6',['build_doxygen_documentation',['../namespacebuild__doxygen__documentation.html',1,'']]],
-  ['build_5fdoxygen_5fdocumentation_2epy_7',['build_doxygen_documentation.py',['../build__doxygen__documentation_8py.html',1,'']]],
-  ['build_5fsphinx_5fdocumentation_8',['build_sphinx_documentation',['../namespacebuild__sphinx__documentation.html',1,'']]],
-  ['build_5fsphinx_5fdocumentation_2epy_9',['build_sphinx_documentation.py',['../build__sphinx__documentation_8py.html',1,'']]]
-];
--- docs/search/all_7.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['function_5fdispatcher_24',['function_dispatcher',['../namespacelib__python_1_1function__dispatcher.html',1,'lib_python']]],
-  ['lib_5fpython_25',['lib_python',['../namespacelib__python.html',1,'']]],
-  ['logger_26',['logger',['../namespacelib__python_1_1loggerutils_1_1logger.html',1,'lib_python::loggerutils']]],
-  ['logger_2epy_27',['logger.py',['../logger_8py.html',1,'']]],
-  ['loggerutils_28',['loggerutils',['../namespacelib__python_1_1loggerutils.html',1,'lib_python']]],
-  ['unix_5ftree_5fcommand_29',['unix_tree_command',['../namespacelib__python_1_1unix__tree__command.html',1,'lib_python']]]
-];
