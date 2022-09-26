ef9c931ed95f534670b9b83a373686e1e005418c
Arnold Tharrington <arnoldt100@gmail.com>
2022-09-26 16:11:38 -0400

    Initial tracking of files "ConsoleLoggingTask.{h,cpp}".
    This files implement class ConsoleLoggingTask which write to stdout.

---
+++ sources/molecular_dynamics/md_taskgroups/header_files/ConsoleLoggingTask.h
@@ -0,0 +1,74 @@
+#ifndef ANANSI_ConsoleLoggingTask_INC
+#define ANANSI_ConsoleLoggingTask_INC
+//! @file ConsoleLoggingTask.h
+//!
+//! Implements logging to the console (i.e. stdout).
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "LoggingTask.h"
+
+namespace ANANSI
+{
+
+// =====================================================================================
+//        Class:  ConsoleLoggingTask
+//  Description:  
+//  =====================================================================================
+class ConsoleLoggingTask : public LoggingTask
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        //--------------------------------------------------------------------------------------
+        //       Class:  ConsoleLoggingTask
+        //      Method:  ConsoleLoggingTask :: ConsoleLoggingTask
+        // Description:  
+        // 
+        //  Parameters: 
+        //
+        //      Return:
+        //--------------------------------------------------------------------------------------
+        ConsoleLoggingTask ();   // constructor
+
+        ConsoleLoggingTask (const ConsoleLoggingTask & other);   // copy constructor
+
+        ConsoleLoggingTask (ConsoleLoggingTask && other);   // copy-move constructor
+
+        virtual ~ConsoleLoggingTask ();  // destructor
+
+        // ====================  ACCESSORS     =======================================
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        ConsoleLoggingTask& operator= ( const ConsoleLoggingTask &other ); // assignment operator
+
+        ConsoleLoggingTask& operator= ( ConsoleLoggingTask && other ); // assignment-move operator
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class ConsoleLoggingTask  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_ConsoleLoggingTask_INC
