36387b242f3fec6fe7ae98d647bada60f956b18b
Arnold Tharrington <arnoldt100@gmail.com>
2022-10-04 14:15:31 -0400

    Initial tracking of receiver.cmake
    Important macros that enable building of the receiver library.

---
+++ sources/core_packages/receiver/cmake_macros_and_functions/receiver.cmake
@@ -0,0 +1,38 @@
+# =====================================================================================
+# 
+#        Filename:  receiver.cmake
+# 
+#     Description:  
+# 
+#          Author:  Arnold N. Tharrington (), arnoldt@ornl.gov
+#    Organization:  ORNL-National Center of Computational Sciences
+# 
+#  =====================================================================================
+
+
+# ===  MACRO     ======================================================================
+#         Name:  enable_building_receiver_package
+#  Description:  Defines key environmental variables enabling the
+#                building of the receiver state library.
+# 
+#  Arguments: 
+#             install_lib_directory - The location to install the actual library
+#             install_include_directory - The location to install header files
+#             install_bin_directory - The location to install binaraies
+#             cxx_standard - The C++ standard to build the library and binaries
+#             logfilepath - The path to the logging file.
+# =====================================================================================
+macro ( enable_building_receiver_package install_lib_directory
+        install_include_directory
+        install_bin_directory 
+        cxx_standard
+        logfilepath)
+
+    set(receiver_install_lib_directory ${install_lib_directory})
+    set(receiver_install_include_directory ${install_include_directory})
+    set(receiver_install_bin_directory ${install_bin_directory} )
+    set(receiver_cxx_standard ${cxx_standard} )
+    set(receiver_logfilepath ${logfilepath})
+endmacro()
+
+
