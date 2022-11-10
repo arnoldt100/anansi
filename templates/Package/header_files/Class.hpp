e9303d72d1b08f9300929d588bcef98a600581ab
Arnold Tharrington <arnoldt100@gmail.com>
2022-11-10 10:53:06 -0500

    Added a new template class.

---
+++ templates/Package/header_files/Class.hpp
@@ -0,0 +1,96 @@
+#ifndef __filepreprocessordefine__
+#define __filepreprocessordefine__
+//! @file __filename__
+//!
+//! Brief description
+//!
+//! Detailed description
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace __NAMESPACE__
+{
+
+// =====================================================================================
+//        Class:  __classname__
+//  Description:  
+//  =====================================================================================
+class __classname__
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        __classname__ ()   // constructor
+        {
+        	// TODO Auto-generated constructor stub
+            return;
+        }
+
+        __classname__ (const __classname__ & other)   // copy constructor
+        {
+        	// TODO Auto-generated constructor stub
+            if (this != &other)
+            {
+                
+            }
+            return;
+        }
+
+        __classname__ (__classname__ && other);   // copy-move constructor
+
+        virtual ~__classname__ ();  // destructor
+        {
+        	// TODO Auto-generated destructor stub
+            return;
+        }
+
+        // ====================  ACCESSORS     =======================================
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        __classname__& operator= ( const __classname__ &other ) // assignment operator
+        {
+            if (this != &other)
+            {
+        
+            }
+            return *this;
+        } // assignment operator
+
+        __classname__& operator= ( __classname__ && other ) // assignment-move operator
+        {
+            if (this != &other)
+            {
+        
+            }
+            return *this;
+        } // assignment operator
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class __classname__  -----
+
+
+}; // namespace __NAMESPACE__
+
+#endif // __filepreprocessordefine__
