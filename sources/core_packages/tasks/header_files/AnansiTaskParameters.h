b7b3ea040de1fb28f24984bc62d43a9726d795cf
Arnold Tharrington <arnoldt100@gmail.com>
2022-11-13 17:08:20 -0500

    End of workday.

---
+++ sources/core_packages/tasks/header_files/AnansiTaskParameters.h
@@ -0,0 +1,67 @@
+#ifndef  ANANSI_AnansiTaskParameters_INC
+#define  ANANSI_AnansiTaskParameters_INC
+
+//! @file __filename__
+//!
+//! Brief description
+//!
+//! Detailed description
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI
+{
+
+class AnansiTaskParameters
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        AnansiTaskParameters ();   // constructor
+
+        AnansiTaskParameters (const AnansiTaskParameters & other);   // copy constructor
+
+        AnansiTaskParameters (AnansiTaskParameters && other);   // copy-move constructor
+
+        ~AnansiTaskParameters ();  // destructor
+
+
+        // ====================  ALAISES       =======================================
+        using task_size_t = std
+        
+        // ====================  ACCESSORS     =======================================
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        AnansiTaskParameters& operator= ( const AnansiTaskParameters &other ); // assignment operator
+
+        AnansiTaskParameters& operator= ( AnansiTaskParameters && other ); // assignment-move operator
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class AnansiTaskParameters  -----
+
+
+}; // namespace ANANSI
+
+#endif   // ----- #ifndef ANANSI_AnansiTaskParameters_INC  ----- 
