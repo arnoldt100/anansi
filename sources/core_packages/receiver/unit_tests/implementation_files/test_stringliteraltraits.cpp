7de4e5ddaedfefb54b809f701f66c9eac49a40e4
Arnold Tharrington <arnoldt100@gmail.com>
2022-12-09 17:02:40 -0500

    End of workday.

--- sources/core_packages/receiver/unit_tests/implementation_files/test_stringliteraltraits.cpp
+++ sources/core_packages/receiver/unit_tests/implementation_files/test_stringliteraltraits.cpp
@@ -10,13 +10,16 @@
 //--------------------- Package includes -----------------//
 //--------------------------------------------------------//
 #include "test_stringliteraltraits.h"
-
+#include "StringLiteralTraits.hpp"
 namespace ANANSI
 {
 
 void verify_valid_char_array_size ()
 {
-    BOOST_TEST(1 > 2);
+    // StringLiteralTraits::MAX_NM_CHARS sets the the char array size of
+    // variable that stores the name of the task command. It needs to be set to
+    // value > 0.
+    BOOST_TEST(StringLiteralTraits::MAX_NM_CHARS > 0, "StringLiteralTraits::MAX_NM_CHARS <= 0");
     return;
 }   /* -----  end of function verify_valid_char_array_size ----- */
 
