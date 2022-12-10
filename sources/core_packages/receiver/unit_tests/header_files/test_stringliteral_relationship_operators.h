a21044ba55e2285987462ac1a6295b56153f95aa
Arnold Tharrington <arnoldt100@gmail.com>
2022-12-10 16:36:30 -0500

    Initial tracking of new receiver test.

---
+++ sources/core_packages/receiver/unit_tests/header_files/test_stringliteral_relationship_operators.h
@@ -0,0 +1,22 @@
+#ifndef test_stringliteral_relationship_operators
+#define test_stringliteral_relationship_operators
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include <boost/test/unit_test.hpp>
+
+namespace ANANSI
+{
+    void verify_relationship_operator_equal();
+} // namespace ANANSI
+
+#endif // ---- test_stringliteral_relationship_operators ----
