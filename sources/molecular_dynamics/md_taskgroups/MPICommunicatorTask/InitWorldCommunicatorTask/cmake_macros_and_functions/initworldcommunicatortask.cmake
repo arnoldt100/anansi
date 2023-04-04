e77ca4340c3eb0cc09af1f3971bebfe6bd8ab09e
Arnold Tharrington <arnoldt100@gmail.com>
2023-04-04 11:05:31 -0400

    These file now properly reflect the target "initworldcommunicatortask".

--- sources/molecular_dynamics/md_taskgroups/MPICommunicatorTask/InitWorldCommunicatorTask/cmake_macros_and_functions/initworldcommunicatortask.cmake
+++ sources/molecular_dynamics/md_taskgroups/MPICommunicatorTask/InitWorldCommunicatorTask/cmake_macros_and_functions/initworldcommunicatortask.cmake
@@ -1,6 +1,6 @@
 # =====================================================================================
 #
-#       Filename:  initworldcommunicator.cmake
+#       Filename:  initworldcommunicatortask.cmake
 #
 #    Description:  
 #
@@ -10,9 +10,9 @@
 # =====================================================================================
 
 # ===  MACRO     ======================================================================
-#         Name:  enable_building_initworldcommunicator_package
+#         Name:  enable_building_initworldcommunicatortask_package
 #  Description:  Defines key environmental variables enabling the
-#                building of the initworldcommunicator packages.
+#                building of the initworldcommunicatortask packages.
 # 
 #  Arguments: 
 #             install_lib_directory - The location to install the various libraries
@@ -20,16 +20,16 @@
 #             install_bin_directory - The location to install binaraies
 #             cxx_standard - The C++ standard to build the library and binaries
 # =====================================================================================
-macro ( enable_building_initworldcommunicator_package install_lib_directory
+macro ( enable_building_initworldcommunicatortask_package install_lib_directory
         install_include_directory
         install_bin_directory 
         cxx_standard
         logfilepath)
 
-    set(initworldcommunicator_install_lib_directory "${install_lib_directory}")
-    set(initworldcommunicator_install_include_directory "${install_include_directory}")
-    set(initworldcommunicator_install_bin_directory "${install_bin_directory}" )
-    set(initworldcommunicator_cxx_standard "${cxx_standard}" )
-    set(initworldcommunicator_logfilepath "${logfilepath}" )
+    set(initworldcommunicatortask_install_lib_directory "${install_lib_directory}")
+    set(initworldcommunicatortask_install_include_directory "${install_include_directory}")
+    set(initworldcommunicatortask_install_bin_directory "${install_bin_directory}" )
+    set(initworldcommunicatortask_cxx_standard "${cxx_standard}" )
+    set(initworldcommunicatortask_logfilepath "${logfilepath}" )
 endmacro()
 
--- sources/molecular_dynamics/md_taskgroups/MPICommunicatorTask/InitWorldCommunicatorTask/cmake_macros_and_functions/initworldcommunicatortask_build_prerequisites.cmake
+++ sources/molecular_dynamics/md_taskgroups/MPICommunicatorTask/InitWorldCommunicatorTask/cmake_macros_and_functions/initworldcommunicatortask_build_prerequisites.cmake
@@ -1,9 +1,9 @@
 # =====================================================================================
 #
-#       Filename:  initworldcommunicator_build_prerequisites.cmake
+#       Filename:  initworldcommunicatortask_build_prerequisites.cmake
 #
 #    Description: Contains convenience functions that help verify the
-#                 prerequisites for building subpackages of initworldcommunicator
+#                 prerequisites for building subpackages of initworldcommunicatortask
 #                 are satisfied.
 #
 #
@@ -11,43 +11,43 @@
 
 
 # ===  FUNCTION  ======================================================================
-#         Name:  verify_initworldcommunicator_build_prerequisites_are_met
+#         Name:  verify_initworldcommunicatortask_build_prerequisites_are_met
 #  Description:  Verifies that key variables that control the building and installation
-#                of the package initworldcommunicator.
+#                of the package initworldcommunicatortask.
 # 
 #   Parameters: 
 #
 #        Return:
 # =====================================================================================
-function(verify_initworldcommunicator_build_prerequisites_are_met)
+function(verify_initworldcommunicatortask_build_prerequisites_are_met)
 
-    # Ensure that variable "initworldcommunicator_logfilepath" is defined.
-    if (NOT DEFINED initworldcommunicator_logfilepath) 
-        message (FATAL_ERROR "initworldcommunicator_logfilepath is not defined.")
+    # Ensure that variable "initworldcommunicatortask_logfilepath" is defined.
+    if (NOT DEFINED initworldcommunicatortask_logfilepath) 
+        message (FATAL_ERROR "initworldcommunicatortask_logfilepath is not defined.")
     endif()
 
-    set(my_message "Verifying \"initworldcommunicator' build prerequisites\" are satisfied.")
-    log_message_to_file( ${initworldcommunicator_logfilepath} "---")
-    log_message_to_file( ${initworldcommunicator_logfilepath} ${my_message})
+    set(my_message "Verifying \"initworldcommunicatortask' build prerequisites\" are satisfied.")
+    log_message_to_file( ${initworldcommunicatortask_logfilepath} "---")
+    log_message_to_file( ${initworldcommunicatortask_logfilepath} ${my_message})
 
-    # Again verfying that variable 'initworldcommunicator_logfilepath' is defined ... repeated
+    # Again verfying that variable 'initworldcommunicatortask_logfilepath' is defined ... repeated
     # for consistency.
-    anansi_test_variable_is_defined("initworldcommunicator_logfilepath" "${initworldcommunicator_logfilepath}")
+    anansi_test_variable_is_defined("initworldcommunicatortask_logfilepath" "${initworldcommunicatortask_logfilepath}")
 
-    # Verfying that variable 'initworldcommunicator_cxx_standard' is defined.
-    anansi_test_variable_is_defined("initworldcommunicator_cxx_standard"  "${initworldcommunicator_logfilepath}")
+    # Verfying that variable 'initworldcommunicatortask_cxx_standard' is defined.
+    anansi_test_variable_is_defined("initworldcommunicatortask_cxx_standard"  "${initworldcommunicatortask_logfilepath}")
 
-    # Verfying that variable 'initworldcommunicator_install_bin_directory' is defined.
-    anansi_test_variable_is_defined("initworldcommunicator_install_bin_directory"  "${initworldcommunicator_logfilepath}")
+    # Verfying that variable 'initworldcommunicatortask_install_bin_directory' is defined.
+    anansi_test_variable_is_defined("initworldcommunicatortask_install_bin_directory"  "${initworldcommunicatortask_logfilepath}")
 
-    # Verfying that variable 'initworldcommunicator_install_include_directory' is defined.
-    anansi_test_variable_is_defined("initworldcommunicator_install_include_directory"  "${initworldcommunicator_logfilepath}")
+    # Verfying that variable 'initworldcommunicatortask_install_include_directory' is defined.
+    anansi_test_variable_is_defined("initworldcommunicatortask_install_include_directory"  "${initworldcommunicatortask_logfilepath}")
 
-    # Verfying that variable 'initworldcommunicator_install_lib_directory' is defined.
-    anansi_test_variable_is_defined("initworldcommunicator_install_lib_directory"  "${initworldcommunicator_logfilepath}")
+    # Verfying that variable 'initworldcommunicatortask_install_lib_directory' is defined.
+    anansi_test_variable_is_defined("initworldcommunicatortask_install_lib_directory"  "${initworldcommunicatortask_logfilepath}")
 
-    set(my_message "Package \"initworldcommunicator' build prerequisities\" are satisfied.")
-    log_message_to_file( ${initworldcommunicator_logfilepath} ${my_message})
-    log_message_to_file( ${initworldcommunicator_logfilepath} "---\n")
-endfunction(verify_initworldcommunicator_build_prerequisites_are_met)
+    set(my_message "Package \"initworldcommunicatortask' build prerequisities\" are satisfied.")
+    log_message_to_file( ${initworldcommunicatortask_logfilepath} ${my_message})
+    log_message_to_file( ${initworldcommunicatortask_logfilepath} "---\n")
+endfunction(verify_initworldcommunicatortask_build_prerequisites_are_met)
 
