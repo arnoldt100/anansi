9bdb5d6a494fbf70ffecde690eac02a57296227c
Arnold Tharrington <arnoldt100@gmail.com>
2023-04-06 15:19:25 -0400

    Transient commit.

--- sources/cmake_macros_and_functions/generic_target_build_prerequisites.cmake
+++ sources/cmake_macros_and_functions/generic_target_build_prerequisites.cmake
@@ -3,26 +3,30 @@
 # variables are defined.                             -
 #                                                    -
 # Parameters: target_name : The name of the target.  -
-#             prerequisite_list : A list of          -
+#             prerequisite_list : The variable name  -
+#             of a list of                           -
 #             prerequisites which will be combined   -
 #             with the target_name to form the       -
 #             to check if defined.                   -
 #                                                    -
 #-----------------------------------------------------
 function(generic_target_build_prerequisites 
-         target_name 
+         target_name
          prerequisite_list)
 
     #-----------------------------------------------------
     # Verify variable log file path variable is          -
     # defined.                                           -
     #-----------------------------------------------------
+
+    # ${target_name}_logfilepath is the vraiable name
+    # To get the value of the file path do ${${target_name}_logfilepath}
     if(NOT DEFINED ${target_name}_logfilepath)
         message( FATAL_ERROR "The variable ${target_name}_logfilepath is not defined.")
     endif()
 
     foreach( tmp_prerequisite ${${prerequisite_list}})
-        anansi_test_variable_is_defined( ${target_name}_${tmp_prerequisite} ${target_name}_logfilepath )
+        anansi_test_variable_is_defined( "${target_name}_${tmp_prerequisite}" "${${target_name}_logfilepath}" )
     endforeach()
 
 
