90e96c21572ed4dce2de46bf6ce10da145b20d53
Arnold Tharrington <arnoldt100@gmail.com>
2023-05-16 20:51:02 -0400

    Transient commit.

---
+++ sources/core_packages/invoker/header_files/ReceiverTypeUtilities.hpp
@@ -0,0 +1,26 @@
+#ifndef ANANSI_ReceiverTypeUtilities_INC
+#define ANANSI_ReceiverTypeUtilities_INC
+//! @file ReceiverTypeUtilities.hpp
+//!
+//! Brief description
+//!
+//! Detailed description
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI
+{
+
+}; // namespace ANANSI
+
+#endif // ANANSI_ReceiverTypeUtilities_INC
