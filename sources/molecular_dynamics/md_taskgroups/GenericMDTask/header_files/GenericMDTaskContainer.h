adc9d874c6a8e6ae3f25ca8f44e144e7637b3f24
arnoldt <arnoldt@ornl.gov>
2023-05-24 08:24:10 -0400

    Transient commit.

---
+++ sources/molecular_dynamics/md_taskgroups/GenericMDTask/header_files/GenericMDTaskContainer.h
@@ -0,0 +1,69 @@
+#ifndef ANANSI_GenericMDTaskContainer_INC
+#define ANANSI_GenericMDTaskContainer_INC
+//! @file GenericMDTaskContainer,h
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+#include <memory>
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "TaskLabel.hpp"
+#include "AnansiTask.h"
+
+namespace ANANSI
+{
+
+//! This class stores the GenericMdTask objects.
+//!
+//! The purpose of this class is to 'wrap' the GenericMdTask objects
+//1 to facilitate modifying the GenericMdTask objects by means of the
+//1 ReceiverInterface::modifyReceiver method which GenericMdTask inherits.
+class GenericMDTaskContainer 
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        GenericMDTaskContainer ();   // constructor
+
+        GenericMDTaskContainer(std::shared_ptr<AnansiTask> my_task);
+
+        GenericMDTaskContainer (const GenericMDTaskContainer & other) = delete;   // copy constructor
+
+        GenericMDTaskContainer (GenericMDTaskContainer && other);   // copy-move constructor
+
+        ~GenericMDTaskContainer ();  // destructor
+
+        // ====================  ACCESSORS     =======================================
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        GenericMDTaskContainer& operator= ( const GenericMDTaskContainer &other )=delete; // assignment operator
+
+        GenericMDTaskContainer& operator= ( GenericMDTaskContainer && other ); // assignment-move operator
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+        std::shared_ptr<AnansiTask> task_;
+
+}; // -----  end of class GenericMDTaskContainer  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_GenericMDTaskContainer_INC
