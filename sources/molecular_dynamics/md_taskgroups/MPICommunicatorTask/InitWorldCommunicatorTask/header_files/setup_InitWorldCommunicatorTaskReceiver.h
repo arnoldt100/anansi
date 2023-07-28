e05a977ac143768fbe23940fe6cb372c5d264ca7
arnoldt <arnoldt@ornl.gov>
2023-07-28 14:22:45 -0400

    Transeint commit for function setup_InitWorldCommunicatorTaskReceiver.

---
+++ sources/molecular_dynamics/md_taskgroups/MPICommunicatorTask/InitWorldCommunicatorTask/header_files/setup_InitWorldCommunicatorTaskReceiver.h
@@ -0,0 +1,24 @@
+#ifndef ANANSI_setup_InitWorldCommunicatorTaskReceiver_INC
+#define ANANSI_setup_InitWorldCommunicatorTaskReceiver_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace ANANSI
+{
+
+void setup_InitWorldCommunicatorTaskReceiver ();
+
+}; /* namespace ANANSI */
+
+#endif // ANANSI_setup_InitWorldCommunicatorTaskReceiver_INC 
