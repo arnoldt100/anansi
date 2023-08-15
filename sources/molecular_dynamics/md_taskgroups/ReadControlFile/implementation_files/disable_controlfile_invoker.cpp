d708967d28e786c0a806957f287d0dbf1821136a
Arnold Tharrington <arnoldt@ornl.gov>
2023-08-15 16:42:42 -0400

    Transient commit.

--- sources/molecular_dynamics/md_taskgroups/ReadControlFile/implementation_files/disable_controlfile_invoker.cpp
+++ sources/molecular_dynamics/md_taskgroups/ReadControlFile/implementation_files/disable_controlfile_invoker.cpp
@@ -18,8 +18,18 @@ void disable_controlfile_invoker (  std::shared_ptr<ANANSI::GenericTaskInvoker<R
                                                     ReadControlFileTraits::concrete_products>
                                                    > & control_file_invoker)
 {
-    // this->mdControlFileInvk_->disableTask(command_labels);
-    return ;
+  // Disable the concrete products of typelist
+  // ReadControlFileTraits::concrete_products in the following order:
+  //      - ControlFileMacroReceiver
+  //      - ControlFileXMLMPICommReceiver
+  //      - ControlFileXMLReceiver
+  const std::vector<ANANSI::TaskLabel> command_labels = {
+      ControlFileXMLReceiver::TASKLABEL,
+      ControlFileXMLMPICommReceiver::TASKLABEL,
+      ControlFileXMLReceiver::TASKLABEL};
+  control_file_invoker->disableTask(command_labels);
+
+  return;
 }   // -----  end of function disable_controlfile_invoker  -----
 
 
