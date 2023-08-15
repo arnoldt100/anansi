b26098941c8751f9ef3b311af8a83ddd449c695f
Arnold Tharrington <arnoldt@ornl.gov>
2023-08-15 17:44:49 -0400

    Transient commit.

---
+++ sources/core_packages/mpi_communicator/header_files/MPIBarrier.h
@@ -0,0 +1,35 @@
+#ifndef ANANSI_MPIBarrier_INC
+#define ANANSI_MPIBarrier_INC
+//! \file MPIBarrier.h
+//!
+//! \brief Brief description
+//!
+//! Detailed description
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+#include "mpi.h"
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI
+{
+class MPIBarrier
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+    static void Barrier(const MPI_Comm aCommunicator);
+
+}; // -----  end of class MPIBarrier  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_MPIBarrier_INC
