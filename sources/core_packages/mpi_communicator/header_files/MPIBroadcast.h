d6d93731d2ec340f06aa11e9ef5d7feb447474f7
arnoldt <arnoldt@ornl.gov>
2023-08-22 10:58:12 -0400

    Added documentation to "MPIBroadcast<STRING_UTILITIES::VectorStringCache>::MPI_Broadcast".

--- sources/core_packages/mpi_communicator/header_files/MPIBroadcast.h
+++ sources/core_packages/mpi_communicator/header_files/MPIBroadcast.h
@@ -1,3 +1,5 @@
+//! \file MPI_Broadcast
+
 #ifndef  ANANSI_MPIBroadcast_INC
 #define  ANANSI_MPIBroadcast_INC
 
@@ -56,11 +58,19 @@ class MPI_Broadcast<std::string>
 };
 
 
+//! \brief Broadcasts thes VectorStringCache object "data_to_broadcast" from the processor with rank "bcast_rank"
+//!        to the remaining ranks in MPI communicator group "mpi_comm".
+//!
+//! \param[in] data_to_broadcast This object caches (or stores) a vector of strings and is broadcasted.
+//! \param[in] mpi_comm The MPI communicator group involved in the broadcast.
+//! \param[in] bcast_rank The MPI broadcast rank.
+//! \return Returns an object of type VectorStringCache which is the broadcasted object "data_to_broadcast" from
+//!         MPI rank "bcast_rank".
 template<>
 class MPI_Broadcast<STRING_UTILITIES::VectorStringCache>
 {
     public:
-    static  STRING_UTILITIES::VectorStringCache Broadcast(const STRING_UTILITIES::VectorStringCache,
+    static  STRING_UTILITIES::VectorStringCache Broadcast(const STRING_UTILITIES::VectorStringCache data_to_broadcast,
                                                           const MPI_Comm mpi_comm,
                                                           const std::size_t bcast_rank);
 };
