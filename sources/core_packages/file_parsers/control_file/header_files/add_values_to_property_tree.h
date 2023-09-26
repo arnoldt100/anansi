fff06ec6997aa681b25934cb31b08ba3858548f7
Arnold Tharrington <arnoldt@ornl.gov>
2023-09-26 13:42:06 -0400

    Renamed a source  file.

--- sources/core_packages/file_parsers/control_file/header_files/add_values_to_property_tree.h
+++ sources/core_packages/file_parsers/control_file/header_files/add_values_to_property_tree.h
@@ -10,12 +10,13 @@
 //--------------------------------------------------------//
 //-------------------- External Library Files ------------//
 //--------------------------------------------------------//
+#include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include  <boost/algorithm/string/trim.hpp>
 
 //--------------------------------------------------------//
 //--------------------- Package includes -----------------//
 //--------------------------------------------------------//
-#include "MasterControlFileNodeKeys.h"
 
 namespace ANANSI
 {
@@ -28,10 +29,32 @@ namespace ANANSI
 //! \param[in] source_ptree The property_tree to copy values from.
 //! \param[in] default_value The default value for the property tree.
 //! \param[out] dest_ptree The property_tree to be filled in.
-void add_values_to_property_tree (const MasterControlFileNodeKeys & master_node_keys,
-                                  const boost::property_tree::ptree & source_pt,
+template<typename T>
+void add_values_to_property_tree (const T & master_node_keys,
+                                  const boost::property_tree::ptree & source_ptree,
                                   const std::string default_value,
-                                  boost::property_tree::ptree & dest_ptree);
+                                  boost::property_tree::ptree & dest_ptree)
+{
+    dest_ptree.clear();
+    const auto  it_keys = master_node_keys.allKeysIterator();
+    for (auto it = it_keys.first; it != it_keys.second; ++it)
+    {
+        const std::string key(*it);
+
+        // Check if this is a comment tag. If a comment tag then ignore
+        // and continue to next key.
+        if ( master_node_keys.isCommentTag(key))
+        {
+           continue; 
+        }
+
+        std::string data = source_ptree.get(key,default_value);
+        boost::algorithm::trim(data);
+        dest_ptree.put(key.c_str(), std::as_const(data));
+    }
+
+    return ;
+}   // -----  end of function add_values_to_property_tree  -----
 
 }; // namespace ANANSI
 
--- sources/core_packages/file_parsers/control_file/implementation_files/add_values_to_property_tree.cpp
+++ sources/core_packages/file_parsers/control_file/implementation_files/add_values_to_property_tree.cpp
@@ -6,43 +6,14 @@
 //--------------------------------------------------------//
 //-------------------- External Library Files ------------//
 //--------------------------------------------------------//
-#include <boost/property_tree/xml_parser.hpp>
-#include  <boost/algorithm/string/trim.hpp>
 
 //--------------------------------------------------------//
 //--------------------- Package includes -----------------//
 //--------------------------------------------------------//
-#include "add_values_to_property_tree.h"
+#include "add_values_to_property_tree.hpp"
 
 namespace ANANSI
 {
-
-void add_values_to_property_tree (const MasterControlFileNodeKeys & master_node_keys,
-                                  const boost::property_tree::ptree & source_pt,
-                                  const std::string default_value,
-                                  boost::property_tree::ptree & dest_pt)
-{
-    dest_pt.clear();
-    const auto  it_keys = master_node_keys.allKeysIterator();
-    for (auto it = it_keys.first; it != it_keys.second; ++it)
-    {
-        const std::string key(*it);
-
-        // Check if this is a comment tag. If a comment tag then ignore
-        // and continue to next key.
-        if ( master_node_keys.isCommentTag(key))
-        {
-           continue; 
-        }
-
-        std::string data = source_pt.get(key,default_value);
-        boost::algorithm::trim(data);
-        dest_pt.put(key.c_str(), std::as_const(data));
-    }
-
-    return ;
-}   // -----  end of function add_values_to_property_tree  -----
-
 
 }; // namespace ANANSI
 
