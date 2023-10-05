45f97e3292fa7c1e98d3292a5de946dae4594be0
Arnold Tharrington <arnoldt@ornl.gov>
2023-10-05 14:07:11 -0400

    Adding KeyPathSeparator.cpp and KeyPathSeparator.h to index.

---
+++ sources/core_packages/file_parsers/file_parsers_core/header_files/KeyPathSeparator.h
@@ -0,0 +1,63 @@
+#ifndef ANANSI_KeyPathSeparator_INC
+#define ANANSI_KeyPathSeparator_INC
+//! \file KeyPathSeparator.h
+//!
+//! \brief Brief description
+//!
+//! Detailed description
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace ANANSI
+{
+
+class KeyPathSeparator
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        KeyPathSeparator ();   // constructor
+
+        KeyPathSeparator (const KeyPathSeparator & other);   // copy constructor
+
+        KeyPathSeparator (KeyPathSeparator && other);   // copy-move constructor
+
+        ~KeyPathSeparator ();  // destructor
+
+        // ====================  ACCESSORS     =======================================
+        KeyPathSeparator * clone () const;
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        KeyPathSeparator& operator= ( const KeyPathSeparator &other ); // assignment operator
+
+        KeyPathSeparator& operator= ( KeyPathSeparator && other ); // assignment-move operator
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class KeyPathSeparator  -----
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_KeyPathSeparator_INC
