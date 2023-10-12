4e6352c9bf73aa5e780044de18601855830f61c6
Arnold Tharrington <arnoldt@ornl.gov>
2023-10-12 16:28:09 -0400

    Transient commit.

---
+++ sources/core_packages/file_parsers/file_parsers_core/header_files/check_string_for_separator_char.h
@@ -0,0 +1,28 @@
+#ifndef ANANSI_check_string_for_separator_char_INC
+#define ANANSI_check_string_for_separator_char_INC
+
+//! \file check_string_for_separator_char.h
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace ANANSI
+{
+
+//! \brief Stud text for brief description
+void check_string_for_separator_char ();
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_check_string_for_separator_char_INC
