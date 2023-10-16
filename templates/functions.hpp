35b21e170688a3960e97170f9d4de9f982d07f88
Arnold Tharrington <arnoldt@ornl.gov>
2023-10-16 13:30:24 -0400

    Adding new template files for generating template functions.

---
+++ templates/functions.hpp
@@ -0,0 +1,31 @@
+#ifndef __filepreprocessordefine__
+#define __filepreprocessordefine__
+
+//! \file __function__.hpp
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace __NAMESPACE__
+{
+
+//! \brief Stud text for brief description
+template< <+template_parameters+> >
+void __function__ ( <+argument_list+> )
+{
+        return;
+}
+
+}; // namespace __NAMESPACE__
+
+#endif // __filepreprocessordefine__
