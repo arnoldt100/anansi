dd27e077951807efb6a2dba6e713842bd5328959
arnoldt <arnoldt@ornl.gov>
2023-11-25 13:40:45 -0500

    End of workday on RuyLopez.

---
+++ sources/core_packages/file_parsers/file_parsers_core/unit_tests/header_files/compare_pickled_files.h
@@ -0,0 +1,30 @@
+#ifndef ANANSI_compare_pickled_files_INC
+#define ANANSI_compare_pickled_files_INC
+
+//! \file compare_pickled_files.h
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+#include <map>
+#include <string>
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace ANANSI
+{
+
+//! \brief Stud text for brief description
+void compare_pickled_files (std::map<> );
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_compare_pickled_files_INC
