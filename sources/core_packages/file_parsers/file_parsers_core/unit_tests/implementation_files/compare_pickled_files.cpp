aabce0507bd7f60754acaff4f75536e441ebbc4d
arnoldt <arnoldt@ornl.gov>
2023-11-29 10:25:17 -0500

    End of workday on Catalan.

--- sources/core_packages/file_parsers/file_parsers_core/unit_tests/implementation_files/compare_pickled_files.cpp
+++ sources/core_packages/file_parsers/file_parsers_core/unit_tests/implementation_files/compare_pickled_files.cpp
@@ -1,6 +1,7 @@
 //--------------------------------------------------------//
 //-------------------- System includes -------------------//
 //--------------------------------------------------------//
+#include <tuple>
 
 //--------------------------------------------------------//
 //-------------------- External Library Files ------------//
@@ -17,6 +18,15 @@ namespace ANANSI
 bool compare_pickled_files (const std::map<std::string,std::string> & correct_pickle_file,
                             const std::map<std::string,std::string> & pickled_file)
 {
+    // This holds the 
+    std::tuple<bool,std::string> ret_value;
+
+    // Verify that map correct_pickle_file and pickled_file are equal. If they
+    // are not equal, then find their difference. 
+    if (correct_pickle_file == pickled_file)
+    {
+
+    } 
     return false;
 }   // -----  end of function compare_pickled_files  -----
 
