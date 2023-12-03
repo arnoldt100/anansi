7ad0b62f06bf857d2682c208bc139a60e8cacaa5
arnoldt <arnoldt@ornl.gov>
2023-12-03 15:44:02 -0500

    Transient commit for implementing test_mpi_communicator.

---
+++ sources/core_packages/mpi_communicator/unit_tests/header_files/test_mpi_communicator.h
@@ -0,0 +1,28 @@
+#ifndef ANANSI_test_mpi_communicator_INC
+#define ANANSI_test_mpi_communicator_INC
+
+//! \file test_mpi_communicator.h
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace ANANSI
+{
+
+//! \brief Stud text for brief description
+void test_mpi_communicator ( <+argument_list+> );
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_test_mpi_communicator_INC
