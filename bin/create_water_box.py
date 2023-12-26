c7b9b8f0ec15d417c76538ff2f071d6a61f88ecf
arnoldt <arnoldt@ornl.gov>
2023-12-26 15:01:26 -0500

    Initial indexing of 'create_water_box.py'.

---
+++ bin/create_water_box.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+## @package create_water_box
+
+# System imports
+import string
+import argparse
+
+# Local imports
+from loggerutils.logger import create_logger_description
+from loggerutils.logger import create_logger
+
+## @fn main ()
+## @brief The main function.
+def main():
+    args = _parse_arguments()
+
+    logger = _create_logger(log_id='main_logger',
+                           log_level=args.log_level)
+
+    logger.info("Start of main program")
+
+    logger.info("End of main program")
+
+if __name__ == "__main__":
+    main()
+
+## @fn _parse_arguments( )
+## @brief Parses the command line arguments.
+##
+## @details Parses the command line arguments and
+## returns A namespace.
+##
+## @return A namespace. The namespace contains attributes
+##         that are the command line arguments.
+def _parse_arguments():
+
+    # Create a string of the description of the 
+    # program
+    program_description = "This program creates water boxes for Anansi." 
+
+    # Create an argument parser.
+    my_parser = argparse.ArgumentParser(
+            description=program_description,
+            formatter_class=argparse.RawTextHelpFormatter,
+            add_help=True)
+
+    # Add an optional argument for the logging level.
+    my_parser.add_argument("--log-level",
+                           type=int,
+                           default=logging.WARNING,
+                           help=create_logger_description() )
+
+    my_args = my_parser.parse_args()
+
+    return my_args 
+
