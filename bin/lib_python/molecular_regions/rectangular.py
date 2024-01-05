9c5046aeb6f5b2605a5a02bc1ae74d3a406d332c
Arnold Tharrington <arnoldt@ornl.gov>
2024-01-05 14:19:09 -0500

    Transient commit.

--- bin/lib_python/molecular_regions/rectangular.py
+++ bin/lib_python/molecular_regions/rectangular.py
@@ -31,6 +31,8 @@ def _indent(elem, level=0):
          elem.tail = i
 
 class Rectangular(Region):
+
+    #  ====================  LIFECYCLE     =======================================
     def __init__(self, 
                  filename,
                  origin=np.array([0.00,0.00,0.00]), 
@@ -40,6 +42,12 @@ class Rectangular(Region):
         self._origin = origin
         self._dimensions = dimensions
         self._pbc = pbc
+
+    #  ====================  PUBLIC        =======================================
+
+    #  ====================  PROTECTED     =======================================
+
+    #  ====================  PRIVATE       =======================================
 
     __XML_INDENT = "    "
 
@@ -67,25 +75,56 @@ class Rectangular(Region):
 
         return my_element
 
-    def _bounding_region_xml_element(self):
-        pass
+    def __bounding_region_xml_element(self):
+        my_element = ET.Element("bounding region") 
+
+        my_low = lambda a : self._origin[a]
+        my_high = lambda a : self._origin[a] + self._dimensions[a]
+
+        x_low_value = my_low(0)
+        x_high_value = my_high(0)
+        x_lower_element = ET.Element("x-lower") 
+        x_lower_element.text = f"""{x_low_value}"""
+        x_upper_element = ET.Element("x-upper") 
+        x_upper_element.text = f"""{x_high_value}"""
+
+        y_low_value = my_low(1)
+        y_high_value = my_high(1)
+        y_lower_element = ET.Element("y-lower") 
+        y_lower_element.text = f"""{y_low_value}"""
+        y_upper_element = ET.Element("y-upper") 
+        y_upper_element.text = f"""{y_high_value}"""
+
+        z_low_value = my_low(2)
+        z_high_value = my_high(2)
+        z_lower_element = ET.Element("z-lower") 
+        z_lower_element.text = f"""{z_low_value}"""
+        z_upper_element = ET.Element("z-upper") 
+        z_upper_element.text = f"""{z_high_value}"""
+
+        my_element.append(x_lower_element)
+        my_element.append(x_upper_element)
+        my_element.append(y_lower_element)
+        my_element.append(y_upper_element)
+        my_element.append(z_lower_element)
+        my_element.append(z_upper_element)
+
+        return my_element
 
     def _boundary_conditions_xml_element(self):
         pass
 
     def _type_of_coordinate_system_element(self):
-        my_element = ET.Element("Type of Coordinate System") 
+        my_element = ET.Element("Coordinate System") 
         my_element.text = self.TYPE_OF_COORDINATE_SYSTEM
+
+        # Append the bounding region element.
+        bounding_region_element = self.__bounding_region_xml_element()
+        my_element.append(bounding_region_element)
 
         # Append the origin element
         my_origin_element = self._origin_xml_element()
         my_element.append(my_origin_element)
-
-        stud_element = ET.Element("Stud") 
-        stud_element.text = f"""Stud element text"""
-        my_element.append(stud_element)
-
-
 
         return my_element
 
