77e144493bf4501cca04ddfa3ecfc19a93ba48cd
Arnold Tharrington <arnoldt@ornl.gov>
2024-01-25 14:05:31 -0500

    Transient commit.

--- sources/molecular_dynamics/md_taskgroups/ReadControlFile/implementation_files/MasterControlInputFileParameters.cpp
+++ sources/molecular_dynamics/md_taskgroups/ReadControlFile/implementation_files/MasterControlInputFileParameters.cpp
@@ -87,13 +87,13 @@ SimulationDecompositionParameters MasterControlInputFileParameters::GetSimulatio
     const std::string wd_type = get_value_CommandFile(my_copy_results,std::string("Simulation_Workload_Decomposition_Type"));
 
     // Get the value for the lattice type for the processor topology.
-    // const std::string pt_mpi_lattice_type = get_value_CommandFile(my_copy_results,std::string("processor-topology.lattice-type"));
+    const std::string pt_mpi_lattice_type = get_value_CommandFile(my_copy_results,std::string("Simulation_Processor_Lattice_Topology"));
 
     // Get the value for the processor topology mpi spatial decomposition.
-    // const std::string pt_mpi_spatial_decompostion = get_value_CommandFile(my_copy_results,std::string("processor-topology.mpi-spatial-decomposition"));
+    const std::string pt_mpi_spatial_decompostion = get_value_CommandFile(my_copy_results,std::string("Simulation_Processor_Spatial_Decomposition"));
 
     // Get the value for the processor topology number of compute units per spatial domain.
-    // const std::string pt_number_cu_per_domain = get_value_CommandFile(my_copy_results,std::string("processor-topology.compute-units-per-spatial-domain"));
+    const std::string pt_number_cu_per_domain = get_value_CommandFile(my_copy_results,std::string("Simulation_Processor_Compute_Units_Per_Spatial_Domain"));
 
     return sim_decomposition_parameters;
 }
