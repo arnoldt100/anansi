bf5b9d74d8f88af9840632ec2ad68699fe2def90
arnoldt <arnoldt@ornl.gov>
2024-02-02 00:04:54 -0500

    End of workday on Catalan.

--- sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/ProcessorTopologyLatticeTypeHelpers.cpp
+++ sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/ProcessorTopologyLatticeTypeHelpers.cpp
@@ -16,11 +16,38 @@ namespace ANANSI
 namespace SDPConstructorHelpers 
 {
 
-
 std::string parse_processor_topology_lattice_type(const std::string a_string,
                                                   const std::string flag_default_null_value)
 {
     std::string my_processor_topology_lattice_type = "";
+    try
+    {
+        if ( (a_string == flag_default_null_value) && 
+             SimulationDecompositionParameters::ProcessorTopologyLatticeTypeKeyIsMandatory()  )
+        {
+            std::string error_message = SimulationDecompositionParameters::ErrorMessageMissingMandatoryNodeTag("processor topology lattice type");
+            throw ErrorMissingProcessorTopologyLatticeTypeParameters(error_message);
+        }
+        else if (SimulationDecompositionParameters::validProcessorTopologyLatticeTypeValues.contains(a_string))
+        {
+            my_workload_decomposition_type = SimulationDecompositionParameters::validWorkLoadDecompositionValues.at(a_string);
+        }
+        else
+        {
+            std::string error_message = SimulationDecompositionParameters::ErrorMessageInvalidProcessorTopologyLatticeTypeValues(a_string);
+            throw ErrorInvalidProcessorTopologyLatticeType(error_message);
+        }
+    }
+    catch (const ErrorMissingProcessorTopologyLatticeTypeParameters & my_error)
+    {
+            const std::string error_message{my_error.what()};
+            throw MOUSEION::GenericErrorClass<SimulationDecompositionParameters>(error_message); 
+    }
+    catch (const ErrorInvalidProcessorTopologyLatticeType & my_error)
+    {
+            const std::string error_message{my_error.what()};
+            throw MOUSEION::GenericErrorClass<SimulationDecompositionParameters>(error_message); 
+    }
     return my_processor_topology_lattice_type; 
 }
 
--- sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/WorkloadDecompositionTypeHelpers.cpp
+++ sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/WorkloadDecompositionTypeHelpers.cpp
@@ -21,46 +21,44 @@ namespace ANANSI
 {
 namespace SDPConstructorHelpers
 {
-    std::string workload_decomposition_type (const std::string a_string,
-                                             const std::string flag_default_null_value)
+
+std::string workload_decomposition_type (const std::string a_string,
+                                         const std::string flag_default_null_value)
+{
+    std::string my_workload_decomposition_type = "";
+    try 
     {
-        std::string my_workload_decomposition_type = "";
-        try 
+
+        if ( (a_string == flag_default_null_value) && 
+             SimulationDecompositionParameters::WorkLoadDecompositionKeyIsMandatory()  )
         {
-
-            if ( (a_string == flag_default_null_value) && 
-                 SimulationDecompositionParameters::WorkLoadDecompositionKeyIsMandatory()  )
-            {
-                std::string error_message = SimulationDecompositionParameters::ErrorMessageMissingMandatoryNodeTag("workload decomposition type");
-                throw ErrorMissingSimulationWorkloadDecompositionParameters(error_message);
-            }
-            else if (SimulationDecompositionParameters::validWorkLoadDecompositionValues.contains(a_string))
-            {
-                my_workload_decomposition_type = SimulationDecompositionParameters::validWorkLoadDecompositionValues.at(a_string);
-            }
-            else
-            {
-                std::string error_message = SimulationDecompositionParameters::ErrorMessageInvalidWorkloadDecompositionNodeValue(a_string);
-                throw ErrorInvalidSimulationWorkloadDecompositionType(error_message);
-            }
+            std::string error_message = SimulationDecompositionParameters::ErrorMessageMissingMandatoryNodeTag("workload decomposition type");
+            throw ErrorMissingSimulationWorkloadDecompositionParameters(error_message);
         }
-        catch (const ErrorMissingSimulationWorkloadDecompositionParameters & my_error) 
+        else if (SimulationDecompositionParameters::validWorkLoadDecompositionValues.contains(a_string))
         {
-            const std::string error_message{my_error.what()};
-            throw MOUSEION::GenericErrorClass<SimulationDecompositionParameters>(error_message); 
+            my_workload_decomposition_type = SimulationDecompositionParameters::validWorkLoadDecompositionValues.at(a_string);
         }
-        catch (const ErrorInvalidSimulationWorkloadDecompositionType & my_error) 
+        else
         {
-            const std::string error_message{my_error.what()};
-            throw MOUSEION::GenericErrorClass<SimulationDecompositionParameters>(error_message); 
+            std::string error_message = SimulationDecompositionParameters::ErrorMessageInvalidWorkloadDecompositionNodeValue(a_string);
+            throw ErrorInvalidSimulationWorkloadDecompositionType(error_message);
         }
-
-        return my_workload_decomposition_type;
-
-       // -----  end of function workload_decomposition_type -----
-
-
     }
+    catch (const ErrorMissingSimulationWorkloadDecompositionParameters & my_error) 
+    {
+        const std::string error_message{my_error.what()};
+        throw MOUSEION::GenericErrorClass<SimulationDecompositionParameters>(error_message); 
+    }
+    catch (const ErrorInvalidSimulationWorkloadDecompositionType & my_error) 
+    {
+        const std::string error_message{my_error.what()};
+        throw MOUSEION::GenericErrorClass<SimulationDecompositionParameters>(error_message); 
+    }
+    return my_workload_decomposition_type;
+
+ 
+}  // -----  end of function workload_decomposition_type -----
 }; // end of namespace SDPConstructorHelpers 
 }; // end of namespace ANANSI
 
