00731ced4e69b371b28e1de1b3d517d6c1e6ecfc
Arnold Tharrington <arnoldt@ornl.gov>
2024-02-10 15:22:36 -0500

    More work workload_decomposition_type package.

--- sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/workload_decomposition_type/WorkloadDecompositionTypeHelpers.cpp
+++ sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/workload_decomposition_type/WorkloadDecompositionTypeHelpers.cpp
@@ -11,7 +11,6 @@
 //--------------------- Package includes -----------------//
 //--------------------------------------------------------//
 #include "WorkloadDecompositionTypeHelpers.h"
-#include "SimulationDecompositionParameters.h"
 #include "GenericErrorClass.hpp"
 #include "ErrorInvalidSimulationDecompositionParameters.h"
 #include "ErrorMissingSimulationDecompositionParameters.h"
@@ -20,6 +19,9 @@
 
 namespace ANANSI
 {
+
+class SimulationDecompositionParameters;
+
 namespace SDPConstructorHelpers
 {
 
