4139113a4d7a82b5b2ec8973e6924709b49d9c89
Arnold Tharrington <arnoldt@ornl.gov>
2024-02-20 15:54:08 -0500

    Transient commit.

---
+++ sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/header_files/details/simulation_decomposition_task_details/setup_pointatoms_communicator_receiver.h
@@ -0,0 +1,28 @@
+#ifndef ANANSI_setup_pointatoms_communicator_receiver_INC
+#define ANANSI_setup_pointatoms_communicator_receiver_INC
+
+//! \file setup_pointatoms_communicator_receiver.h
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace ANANSI
+{
+
+//! \brief Setups the receiver  PointAtomsCommunicator.
+void setup_pointatoms_communicator_receiver ();
+
+
+}; // namespace ANANSI
+
+#endif // ANANSI_setup_pointatoms_communicator_receiver_INC
--- sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/setup_simulationdecomposition_invoker.cpp
+++ sources/molecular_dynamics/md_taskgroups/SimulationDecomposition/implementation_files/setup_simulationdecomposition_invoker.cpp
@@ -12,6 +12,7 @@
 #include "setup_simulationdecomposition_invoker.h"
 #include "setup_read_pointatoms_receiver.h"
 #include "setup_pointatoms_decomposer_receiver.h"
+#include "setup_pointatoms_communicator_receiver.h"
 
 namespace ANANSI
 {
@@ -21,8 +22,10 @@ void setup_simulationdecomposition_invoker (const SimulationDecompositionParamet
                                                                                        SimulationDecompositionTaskTraits::concrete_products>
                                                > & simulatiion_decomposer_invoker )
 {
-    setup_read_pointatoms_receiver();
+    setup_pointatoms_communicator_receiver();
     setup_pointatoms_decomposer_receiver();
+    setup_read_pointatoms_receiver();
+
     return;
 }   // -----  end of function setup_simulationdecomposition_invoker  -----
 
