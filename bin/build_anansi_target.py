ca4640d782870725826b0f74163fce7026f2149b
Arnold Tharrington <arnoldt@ornl.gov>
2025-09-03 14:13:45 -0400

    The target build directories are now created with script 'build_anansi_target.py'.

--- bin/build_anansi_target.py
+++ bin/build_anansi_target.py
@@ -12,8 +12,10 @@
 #                     |
 #                     |-----/unit_test_logs
 # 
-# The environmental varable "ANANSI_TARGET_ARCHITECTURE" must
-# be defined or the program will abort.
+# The environmental variable "ANANSI_TARGET_ARCHITECTURE" and "ANANSI_TOP_LEVEL"
+# must be defined or the program will abort with failure.
+# 
+# The directories to be created must not exist prior of the program will abort with failure.
 
 # System imports
 import string
@@ -26,11 +28,45 @@ import logging
 from loggerutils.logger import create_logger_description
 from loggerutils.logger import create_logger
 
+## @fn main ()
+## @brief The main function.
+def main():
+    try:
+        _verify_environmental_variables_set()
+    except _UndefinedCriticalEnvironmentalVariableError as my_error:
+        print(my_error)
+        sys.exit(1)
+
+    args = _parse_arguments()
+
+    logger = create_logger(log_id='__LoggerID__',
+                           log_level=args.log_level)
+
+    logger.info("Start of main program")
+
+    try:
+        _create_target_architecture_directories()
+    except _TargetPathExistsError as my_error:
+        print(my_error)
+        sys.exit(1)
+
+    logger.info("End of main program")
+
 ## @class _UndefinedCriticalEnvironmentalVariableError
 ## @brief This error is thrown when a critical environmental variable is undefined.
 class _UndefinedCriticalEnvironmentalVariableError(Exception):
     def __init__(self,message):
         self._message = f"The crtitical environmental variable '{message}' is undefined."
+        super().__init__(self._message)
+
+    def __str__(self):
+        return self._message
+
+## @class _UndefinedCriticalEnvironmentalVariableError
+## @brief This error is thrown when a critical environmental variable is undefined.
+class _TargetPathExistsError(Exception):
+    def __init__(self,message):
+        self._message = f"The target directory file path '{message}' already exists."
         super().__init__(self._message)
 
     def __str__(self):
@@ -58,13 +94,16 @@ def _verify_environmental_variables_set():
 def _create_target_architecture_directories():
     anansi_top_level = os.getenv("ANANSI_TOP_LEVEL")
     anansi_target_architecture = os.getenv("ANANSI_TARGET_ARCHITECTURE")
+    target_top_level = os.path.join(anansi_top_level,anansi_target_architecture)
 
+    if os.path.exists(target_top_level):
+        raise _TargetPathExistsError(target_top_level)
 
-    directories_to_create = ( os.path.join(anansi_top_level,anansi_target_architecture,'etc'),
-                              os.path.join(anansi_top_level,anansi_target_architecture,'build'),
-                              os.path.join(anansi_top_level,anansi_target_architecture,'configuration'),
-                              os.path.join(anansi_top_level,anansi_target_architecture,'documentation'),
-                              os.path.join(anansi_top_level,anansi_target_architecture,'umit_test_logs') )
+    directories_to_create = ( os.path.join(target_top_level,'etc'),
+                              os.path.join(target_top_level,'build'),
+                              os.path.join(target_top_level,'configuration'),
+                              os.path.join(target_top_level,'documentation'),
+                              os.path.join(target_top_level,'umit_test_logs') )
 
     for tmp_dir in directories_to_create:
         os.makedirs(tmp_dir)
@@ -82,8 +121,8 @@ def _parse_arguments():
 
     # Create a string of the description of the 
     # program
-    program_description =  "This program creates directories and copies configuration files\n" 
-    program_description += "for building the target architecture. The environmental variables\n" 
+    program_description =  "This program creates directories and copies configuration files\n"
+    program_description += "for building the target architecture. The environmental variables\n"
     program_description += "'ANANSI_TARGET_ARCHITECTURE' and 'ANANSI_TOP_LEVEL' must be \n"
     program_description += "defined or the program will abort with failure.\n"
 
@@ -103,25 +142,6 @@ def _parse_arguments():
 
     return my_args
 
-## @fn main ()
-## @brief The main function.
-def main():
-    try:
-        _verify_environmental_variables_set()
-    except _UndefinedCriticalEnvironmentalVariableError as my_error:
-        print(my_error)
-        sys.exit(1)
-
-    args = _parse_arguments()
-
-    logger = create_logger(log_id='__LoggerID__',
-                           log_level=args.log_level)
-
-    logger.info("Start of main program")
-
-    _create_target_architecture_directories()
-
-    logger.info("End of main program")
 
 if __name__ == "__main__":
     main()
