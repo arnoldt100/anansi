6319f2e0644fa7aded34f5032e2c83d1d8f40c86
Arnold Tharrington <arnoldt@ornl.gov>
2025-09-03 12:28:10 -0400

    Start of workday on KingsIndian.

--- bin/build_anansi_target.py
+++ bin/build_anansi_target.py
@@ -1,16 +1,39 @@
 #! /usr/bin/env python3
-## @package __packagename__
-#  __BriefDoxygenDescription__
+## @package build_anansi_target,py
+#  This program creates directories and copies files to build Anansi 
+#  for the targeted architecture,
 #
-# __DetailedDoxygenDescription__
+#  This program creates the directories
+#  ${ANANSI_TOP_LEVEL}|-----/build
+#                     |
+#                     |-----/configurations
+#                     |
+#                     |-----/documentation
+#                     |
+#                     |-----/unit_test_logs
+# 
+# The environmental varable "ANANSI_TARGET_ARCHITECTURE" must
+# be defined or the program will abort.
 
 # System imports
 import string
 import argparse
 
 # Local imports
+import logging
 from loggerutils.logger import create_logger_description
 from loggerutils.logger import create_logger
+
+## @fn _verify_environmental_variables_set( )
+## @brief Verifies critical environmental variables are set.
+##
+## @details If critical environmental variables are not set
+## the progrom will abort.
+##
+## @return None
+def _verify_environmental_variables_set():
+    # We define a list of environmental variables to check if defined.
+    critical_environmental_variables = ["ANANSI_TARGET_ARCHITECTURE"]
 
 ## @fn _parse_arguments( )
 ## @brief Parses the command line arguments.
@@ -24,8 +47,10 @@ def _parse_arguments():
 
     # Create a string of the description of the 
     # program
-    program_description = "This program creates directories and copies configuration files" 
-    program_description += " ...add some more ..."
+    program_description =  "This program creates directories and copies configuration files\n" 
+    program_description += "for building the target architecture. The environmental variable\n" 
+    program_description += "'ANANSI_TARGET_ARCHITECTURE' must be defined or the program will abort.\n"
+# "
     # Create an argument parser.
     my_parser = argparse.ArgumentParser(
             description=program_description,
@@ -45,6 +70,11 @@ def _parse_arguments():
 ## @fn main ()
 ## @brief The main function.
 def main():
+    try:
+        _verify_environmental_variables_set()
+    except:
+        pass
+
     args = _parse_arguments()
 
     logger = create_logger(log_id='__LoggerID__',
