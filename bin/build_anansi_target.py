9ac6bbdc589d71dfc13ab16828c61583d95247ca
Arnold Tharrington <arnoldt@ornl.gov>
2025-09-03 13:53:57 -0400

    Transient commit for build_anansi_target.py
    In progress of creating target directories.

--- bin/build_anansi_target.py
+++ bin/build_anansi_target.py
@@ -18,11 +18,23 @@
 # System imports
 import string
 import argparse
+import os
+import sys
 
 # Local imports
 import logging
 from loggerutils.logger import create_logger_description
 from loggerutils.logger import create_logger
+
+## @class _UndefinedCriticalEnvironmentalVariableError
+## @brief This error is thrown when a critical environmental variable is undefined.
+class _UndefinedCriticalEnvironmentalVariableError(Exception):
+    def __init__(self,message):
+        self._message = f"The crtitical environmental variable '{message}' is undefined."
+        super().__init__(self._message)
+
+    def __str__(self):
+        return self._message
 
 ## @fn _verify_environmental_variables_set( )
 ## @brief Verifies critical environmental variables are set.
@@ -33,7 +45,30 @@ from loggerutils.logger import create_logger
 ## @return None
 def _verify_environmental_variables_set():
     # We define a list of environmental variables to check if defined.
-    critical_environmental_variables = ["ANANSI_TARGET_ARCHITECTURE"]
+    critical_environmental_variables = ["ANANSI_TARGET_ARCHITECTURE",
+                                        "ANANSI_TOP_LEVEL"]
+
+    for variable in critical_environmental_variables:
+        value = os.getenv(variable)
+        if value == None:
+           raise _UndefinedCriticalEnvironmentalVariableError(variable)
+
+## @fn _create_target_architecture_directories( )
+## @brief Creates the directories needed for building the target architecture.
+def _create_target_architecture_directories():
+    anansi_top_level = os.getenv("ANANSI_TOP_LEVEL")
+    anansi_target_architecture = os.getenv("ANANSI_TARGET_ARCHITECTURE")
+
+
+    directories_to_create = ( os.path.join(anansi_top_level,anansi_target_architecture,'etc'),
+                              os.path.join(anansi_top_level,anansi_target_architecture,'build'),
+                              os.path.join(anansi_top_level,anansi_target_architecture,'configuration'),
+                              os.path.join(anansi_top_level,anansi_target_architecture,'documentation'),
+                              os.path.join(anansi_top_level,anansi_target_architecture,'umit_test_logs') )
+
+    for tmp_dir in directories_to_create:
+        os.makedirs(tmp_dir)
+
 
 ## @fn _parse_arguments( )
 ## @brief Parses the command line arguments.
@@ -48,9 +83,10 @@ def _parse_arguments():
     # Create a string of the description of the 
     # program
     program_description =  "This program creates directories and copies configuration files\n" 
-    program_description += "for building the target architecture. The environmental variable\n" 
-    program_description += "'ANANSI_TARGET_ARCHITECTURE' must be defined or the program will abort.\n"
-# "
+    program_description += "for building the target architecture. The environmental variables\n" 
+    program_description += "'ANANSI_TARGET_ARCHITECTURE' and 'ANANSI_TOP_LEVEL' must be \n"
+    program_description += "defined or the program will abort with failure.\n"
+
     # Create an argument parser.
     my_parser = argparse.ArgumentParser(
             description=program_description,
@@ -65,15 +101,16 @@ def _parse_arguments():
 
     my_args = my_parser.parse_args()
 
-    return my_args 
+    return my_args
 
 ## @fn main ()
 ## @brief The main function.
 def main():
     try:
         _verify_environmental_variables_set()
-    except:
-        pass
+    except _UndefinedCriticalEnvironmentalVariableError as my_error:
+        print(my_error)
+        sys.exit(1)
 
     args = _parse_arguments()
 
@@ -81,6 +118,8 @@ def main():
                            log_level=args.log_level)
 
     logger.info("Start of main program")
+
+    _create_target_architecture_directories()
 
     logger.info("End of main program")
 
