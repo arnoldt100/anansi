81def69918d213d8a81ee0cf618d7bb6794c2c2f
Arnold Tharrington <arnoldt@ornl.gov>
2025-09-28 12:27:18 -0400

    Renaming a file.

---
+++ bin/build_anansi_cmake_configure.sh
@@ -0,0 +1,42 @@
+#  /usr/bin/env bash
+
+#-----------------------------------------------------
+# Define the Anansi buld directory.                  -
+#                                                    -
+#-----------------------------------------------------
+declare -r anansi_cmake_build_dir="${ANANSI_TOP_LEVEL}/${ANANSI_TARGET_ARCHITECTURE}/build"
+
+#-----------------------------------------------------
+# The number of make threads.                        -
+#                                                    -
+#-----------------------------------------------------
+declare -r -i NM_MAKE_THREADS=2
+
+#-----------------------------------------------------
+# Define a log file                                  -
+#                                                    -
+#-----------------------------------------------------
+declare -r log_file="${ANANSI_TOP_LEVEL}/${ANANSI_TARGET_ARCHITECTURE}/anansi_log.txt"
+
+#-----------------------------------------------------
+# No edits should be needed below this line.         -
+#                                                    -
+#-----------------------------------------------------
+
+if [ -f ${log_file} ]; then
+  rm -f ${log_file}
+fi
+
+if [ -d ${anansi_cmake_build_dir} ]; then
+  rm -rf ${anansi_cmake_build_dir}
+fi
+mkdir ${anansi_cmake_build_dir}
+cd ${anansi_cmake_build_dir}
+
+cmake ${ANANSI_TOP_LEVEL}/sources \
+  -DCMAKE_CXX_COMPILER=${ANANSI_CMAKE_CXX_COMPILER} \
+  -DCMAKE_C_COMPILER=${ANANSI_CMAKE_C_COMPILER} \
+  -DCMAKE_INSTALL_PREFIX=${ANANSI_INSTALL_PREFIX} \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_PREFIX_PATH="${MPICH_DIR}" \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1
